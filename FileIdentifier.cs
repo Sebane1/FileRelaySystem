65246b110fa37a754c596c5255c6d17c74043c34
Sebastina <sebane1@thavea.com>
2023-06-27 12:10:49 -0500

    Dont dispose if file is in use still.

--- FileIdentifier.cs
+++ FileIdentifier.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FileSystemRelay {
     public class FileIdentifier : IDisposable {
         string identifier;
-        string modName;
         MemoryStream memoryStream;
         bool inUse;
         public event EventHandler OnDisposed;
@@ -19,19 +15,17 @@ namespace FileSystemRelay {
         }
 
         public MemoryStream MemoryStream { get => memoryStream; set => memoryStream = value; }
-        public string ModName { get => modName; set => modName = value; }
         public string Identifier { get => identifier; set => identifier = value; }
         public bool InUse { get => inUse; set => inUse = value; }
 
         public void Dispose() {
             memoryStream.Dispose();
-            modName = null;
             identifier = string.Empty;
         }
         async void DestroyOverTime() {
             Stopwatch stopwatch = Stopwatch.StartNew();
-            while (stopwatch.ElapsedMilliseconds < 30000) {
-                Thread.Sleep(60000);
+            while (stopwatch.ElapsedMilliseconds < 10000 || inUse) {
+                Thread.Sleep(1000);
             }
             Dispose();
             OnDisposed?.Invoke(this, EventArgs.Empty);
