b68629523313af9dd720a1202b40cac4a8ae4cb4
Sebastina <sebane1@thavea.com>
2023-06-29 22:57:56 -0500

    Listen on multiple ports

--- Program.cs
+++ Program.cs
@@ -2,23 +2,25 @@
 
 namespace FileSystemRelay {
     internal class Program {
-        static TcpListener fileReceiverListener = null;
+        static List<TcpListener> fileReceiverListeners = new List<TcpListener>();
         static void Main(string[] args) {
-            fileReceiverListener = new TcpListener(5400);
-            fileReceiverListener.Start();
-            FileManager fileManager = new FileManager();
-            Console.WriteLine("Starting server");
-            Thread thread = new Thread(new ThreadStart(delegate {
-                while (true) {
-                    TcpClient client = fileReceiverListener.AcceptTcpClient();
-                    client.LingerState = new LingerOption(false, 5);
-                    ReceiveFromClientManager receiveFromClientManager = new ReceiveFromClientManager(client, fileManager);
-                    Thread thread = new Thread(new ThreadStart(receiveFromClientManager.ReceiveFromClient));
-                    thread.Start();
-                }
-            }));
-            thread.Start();
-
+            for (int i = 0; i < 10; i = 0) {
+                TcpListener fileReceiverListener = new TcpListener(5105 + ((i + 1) * 100));
+                fileReceiverListener.Start();
+                FileManager fileManager = new FileManager();
+                Console.WriteLine("Starting server");
+                Thread thread = new Thread(new ThreadStart(delegate {
+                    while (true) {
+                        TcpClient client = fileReceiverListener.AcceptTcpClient();
+                        client.LingerState = new LingerOption(false, 5);
+                        ReceiveFromClientManager receiveFromClientManager = new ReceiveFromClientManager(client, fileManager);
+                        Thread thread = new Thread(new ThreadStart(receiveFromClientManager.ReceiveFromClient));
+                        thread.Start();
+                    }
+                }));
+                thread.Start();
+                fileReceiverListeners.Add(fileReceiverListener);
+            }
             Console.WriteLine("Server started");
             while (true) {
                 Thread.Sleep(10000);
